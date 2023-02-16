5067a98a622b08552e985146f53f03fac30e0f1d
Alun <alun.king1@southwales.ac.uk>
2023-02-16 14:06:09 +0000

    Initial Commit

---
+++ SimpleNavigation/SimpleNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNavigationApp.swift
+//  SimpleNavigation
+//
+//  Created by Alun King on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
